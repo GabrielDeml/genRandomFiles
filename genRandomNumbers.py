ef417b0ff2d87404100807f6723a57ef5bdfd382
GabreilDeml <gabrielddeml@gmail.com>
2020-03-23 20:56:52 -0400

    It allmost workes, but I don't quite have the first file working

--- genRandomNumbers.py
+++ genRandomNumbers.py
@@ -5,23 +5,47 @@ import random
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--dir", type=int, default=5, help="Number of directories to make")
+parser.add_argument("-d", "--dir", type=int, default=5, help="Number of dirs at each depth")
 parser.add_argument("-f", "--files", type=int, default=5, help="Max number of files to make")
 parser.add_argument("-l", "--location", type=str, default='.', help="Location to make the folders")
 parser.add_argument("-s", "--size", type=int, default=5, help="Length of file name sizes")
 parser.add_argument("-c", '--contents', type=int, default=50, help="Max number of letters in the file")
+parser.add_argument("--depth", type=int, default=2, help="depth of the file tree")
 args = parser.parse_args()
 root = args.location + "/randomFiles/"
-if not os.path.exists(root):
-    os.mkdir(root)
 
-for d in range(0, args.dir):
-    dirName = ''.join(random.choices(string.ascii_uppercase + string.digits, k=args.size))
-    while not os.path.exists(root + dirName):
-        os.mkdir(root + dirName)
-    for f in range(0, random.randrange(0, args.files)):
-        print("d: " + str(d) + " f: " + str(f))
+
+def gen_files(location, currentDepth):
+    # If we are at the desired depth return
+    if currentDepth == args.depth:
+        return
+    # Generate the dirs
+    for d in range(0, args.dir):
+        dirName = None
+        # Make the file and make sure it does not match a file we already made
+        while dirName is None or (not os.path.exists(location + dirName)):
+            dirName = ''.join(random.choices(string.ascii_uppercase + string.digits, k=args.size))
+            os.mkdir(location + dirName)
+            print("tying to make file: " + location + dirName)
+        # Generate some files
+        makeFiles(location, dirName)
+        # Make the files in this dir recursively
+        gen_files(location + dirName + "/", currentDepth + 1)
+
+
+def makeFiles(location, dirName):
+    for f in range(0, args.files):
         fileName = ''.join(random.choices(string.ascii_uppercase + string.digits, k=args.size))
-        fileContents = ''.join(
-            random.choices(string.ascii_uppercase + string.digits, k=random.randrange(0, args.contents)))
-        print(dirName + " " + fileName + " " + fileContents)
+        fileContents = ''.join(random.choices(string.ascii_uppercase + string.digits, k=args.contents))
+        print("Writing: " + fileContents + " to: " + location + dirName + fileName + ".txt")
+        f = open(location + dirName + '/' + fileName + ".txt", "w")
+        f.write(fileContents)
+        f.close()
+
+
+if __name__ == "__main__":
+    # Make the root folder to put all the generated folders
+    if not os.path.exists(root):
+        os.mkdir(root)
+    # Start the recursion
+    gen_files(root, 0)
